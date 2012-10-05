01af539fd053b74eedc797a16df59a33b5cca46d
sigma <thtsigma@gmail.com>
2012-10-04 22:32:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '581f31a440d533b9c4b75ddfe895e03159d771ff0e318b21297c841fea641a0312815bb8d6c7e8c772497f6be9658a1f5cddf61f72864bf0bec0a6b7906f0a12'
